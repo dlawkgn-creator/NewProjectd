7e0059aa35ecfad854e2bb40c7c74655b80adac6
dlawkgn-creator <dlawkgn1@gmail.com>
2025-12-09 17:35:10 +0900

    기초적인 플레이어 기능 추가
    - 이동 구현 완료
    - 점프 구현 완료
    - 점프시 이동 가속 옵션 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MoveSpeed { get; set; } = 5f;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _jumpHeight = 1f;
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _airMoveMultiplier = 1.1f; //◀검색 결과
+
+    public float MoveSpeed
+    {
+        get => _moveSpeed;
+        set => _moveSpeed = value;
+    }
+
+    public float JumpHeight
+    {
+        get => _jumpHeight;
+        set => _jumpHeight = value;
+    }
+
+    public float Gravity
+    {
+        get => _gravity;
+        set => _gravity = value;
+    }
+
+    public float AirMoveMultiplier
+    {
+        get => _airMoveMultiplier;
+        set => _airMoveMultiplier = value;
+    }
 
     private CharacterController _cc;
     private Vector2 _moveInput;
+    private float _verticalVelocity;
 
     private void Awake()
     {
@@ -18,8 +46,26 @@ public class PlayerController : MonoBehaviour
         _moveInput = value.Get<Vector2>();
     }
 
+    public void OnJump(InputValue value)
+    {
+        if (!value.isPressed)
+        {
+            return;
+        }
+
+        if (_cc.isGrounded && _verticalVelocity <= 0f)
+        {
+            _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+        }
+    }
+
     private void Update()
     {
+        if (_cc.isGrounded && _verticalVelocity < 0f)
+        {
+            _verticalVelocity = -1f;
+        }
+
         Vector3 move = new Vector3(_moveInput.x, 0f, _moveInput.y);
 
         if (move.sqrMagnitude > 0.001f)
@@ -27,6 +73,15 @@ public class PlayerController : MonoBehaviour
             move = transform.TransformDirection(move);
         }
 
-        _cc.Move(move * MoveSpeed * Time.deltaTime);
+        _verticalVelocity += Gravity * Time.deltaTime;
+        move.y = _verticalVelocity;
+
+        float currentSpeed = MoveSpeed;
+        if (!_cc.isGrounded)
+        {
+            currentSpeed *= AirMoveMultiplier;
+        }
+
+        _cc.Move(move * currentSpeed * Time.deltaTime);
     }
 }
